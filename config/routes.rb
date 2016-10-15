f235709ca59d037d12b586556fa70b63cf93f6da
sombek <sombek990@hotmail.com>
2016-10-15 20:35:39 +0300

    Add comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
 
-	resources :posts
+	resources :posts do #nesting the routes of comments all
+											#comments are belong to posts
+		resources :comments
+	end
 	root "posts#index"
 
 end
